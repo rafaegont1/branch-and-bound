86fafad962c8101268a943934db73fcc1ad4ccf3
rafaegont1 <rafaelg000@gmail.com>
2025-07-04 17:03:40 -0300

    parser funcionando; falta implementar o interpretador

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from src.ampl import parse_text
 
 
 def main() -> None:
-    text = "var x1 integer free;"
+    text = """var x1 integer free;
+    var x2 integer free;
+    minimize: 2*x1 + 1*x2;
+    5*x1 + 10*x2 <= 10;"""
+
     parse_text(text)
 
 
--- src/ampl.py
+++ src/ampl.py
@@ -1,45 +1,69 @@
-from lark import Lark, Transformer, UnexpectedToken, v_args
+from lark import Lark, Transformer, v_args
+from lark.lexer import Token
 
 GRAMMAR = """
-    ?start: var
+    ?start: variable+ objective constraint+
+    ?variable: "var" NAME VAR_TYPE VAR_BOUND ";" -> decl_var
+    ?objective: OBJ_SENSE ":" sum ";"            -> obj
+    ?constraint: sum CMP_OP sum ";"              -> st
 
-    ?var: "var" NAME TYPE BOUND ";" -> var
-    TYPE: "integer" | "real"
-    BOUND: "<=0" | ">=0" | "free"
+    ?sum: product
+        | sum "+" product   -> add
+        | sum "-" product   -> sub
+
+    ?product: atom
+        | product "*" atom  -> mul
+
+    ?atom: NUMBER           -> number
+         | "-" atom         -> neg
+         | NAME             -> var
+         | "(" sum ")"
+
+    VAR_TYPE: "integer" | "real"
+    VAR_BOUND: "<=0" | ">=0" | "free"
+    OBJ_SENSE: "minimize" | "maximize"
+    CMP_OP: "<=" | "=" | ">="
 
     %import common.CNAME -> NAME
     %import common.NUMBER
-    %import common.WS_INLINE
+    %import common.WS
 
-    %ignore WS_INLINE
+    %ignore WS
 """
 
 
 @v_args(inline=True)    # Affects the signatures of the methods
 class CalculateTree(Transformer):
-    # from operator import add, sub, mul, truediv as div, neg
-    # number = float
+    from operator import add, sub, mul, neg
+    number = float
 
     def __init__(self) -> None:
         self.vars = {}
 
-    def var(self, name, type, bound):
-        print(name, type, bound)
+    def decl_var(self, name: Token, vtype: Token, bound: Token) -> None:
+        print("---decl_var---")
+        print(type(name), type(vtype), type(bound))
+        print(name, vtype, bound)
+        self.vars[name] = (vtype, bound)
 
-    # def type(self, type_str) -> None:
-    #     print("type")
-    #     print(type_str)
+    def obj(self, sense: Token, expr) -> None:
+        print("---obj---")
+        print(type(sense), type(expr))
+        print(sense, expr)
 
-    # def bound(self, bound_str) -> None:
-    #     print("bound")
-    #     print(bound_str)
+    def st(self, left_expr, cmp_op: Token, right_expr) -> None:
+        print("---st---")
+        print(type(left_expr), type(cmp_op), type(right_expr))
+        print(left_expr, cmp_op, right_expr)
+
+    def var(self, name):
+        print("---var---")
+        print(type(name))
+        print(name)
+        return 1
 
 
 def parse_text(text: str) -> None:
     transformer = CalculateTree()
     parser = Lark(GRAMMAR, parser='lalr', transformer=transformer)
-    try:
-        parser.parse(text)
-    except UnexpectedToken as e:
-        print(f"Parser error: expcted: {e.expected}, found {e.token} ({e.line}:{e.column})")
-    # print(transformer.ident('a'))
+    parser.parse(text)
