6f300a9980c58fe1658643d5dffe7c8e425d63cd
rafaegont1 <rafaelg000@gmail.com>
2025-07-13 11:10:49 -0300

    comecei a implementar a tabela de log

--- src/branch_and_bound.py
+++ src/branch_and_bound.py
@@ -23,62 +23,57 @@ class BranchAndBound:
         self.iteration = 0
         self.there_is_unbounded_solution = False
 
-        # print("núm iter\t\t núm aval\t\tnúm ñ aval\t\tz iter\t\tação\t\tz ótim\t\ttempo (s)")
+        print("iter\t\taval\t\tñ aval\t\tz iter\t\tação\t\tz ótim\t\ttempo (s)")
 
-        self.__iterate(model, int_var_names, 1)
+        self.__iterate(model, int_var_names)
 
         end_time = time()
         elapsed_time = end_time - start_time
         print(f"elapsed time: {elapsed_time:.3f} s")
 
-    def __iterate(self,
-        model: gp.Model,
-        int_var_names: list[str],
-        node_id: int
-    ) -> None:
+    def __iterate(self, model: gp.Model, int_var_names: list[str]) -> None:
         self.iteration += 1
-        print(f"-----> iteração nº: {self.iteration} <-----")
-        print(f"-----> node nº: {node_id} <-----")
+        print(self.iteration, end='\t\t')
+        print("TODO", end='\t\t')  # TODO: nodes avaliados
+        print("TODO", end='\t\t')  # TODO: nodes não avaliados
 
         # Relaxação linear do subproblema
         model.optimize()
-        print(f"status: {model.Status}")
-        print(f"variáveis: {model.getVars()}")
         if model.Status == GRB.OPTIMAL:
-            print(f"z: {model.ObjVal}")
+            print(f"{model.ObjVal:.2f}", end='\t\t')
+        else:
+            print("None", end='\t\t')
         # Variáveis que devem ser inteiras
         y = [model.getVarByName(yi_name) for yi_name in int_var_names]
 
         # Poda por invibialidade
         if model.Status != GRB.OPTIMAL:
-            print(f"iteração {self.iteration}: poda por invibialidade")
+            print("I")
             if model.Status == GRB.UNBOUNDED:
                 self.there_is_unbounded_solution = True
         # Poda pelo limite
         elif model.ObjVal >= self.best_solution.z:
-            print(f"iteração {self.iteration}: poda pelo limite")
+            print("L")
         # Poda por otimalidade
         elif all(yi.X.is_integer() for yi in y):
-            print(f"iteração {self.iteration}: poda por otimalidade")
+            print("O")
             if model.ObjVal < self.best_solution.z:
                 self.best_solution.x = {v.VarName: v.X for v in model.getVars()}
                 self.best_solution.z = model.ObjVal
         # Nenhuma poda possível
         else:
-            print(f"iteração {self.iteration}: nenhuma poda possível")
+            print("D")
 
             # Variável com maior resíduo: a variável inteira que assume valor
             # fracionário e que a parte fracionária é mais próxima de 0.5 é
             # escolhida para realizar a ramificação
             branch_var = min(y, key=lambda yi: abs((yi.X % 1) - 0.5))
 
-            print(f"branching with: {branch_var}")
-
             left_branch = self.__new_branch(model, branch_var, 'left')
-            self.__iterate(left_branch, int_var_names, node_id + 1)
+            self.__iterate(left_branch, int_var_names)
 
             right_branch = self.__new_branch(model, branch_var, 'right')
-            self.__iterate(right_branch, int_var_names, node_id + 2)
+            self.__iterate(right_branch, int_var_names)
 
     def __new_branch(self,
         parent: gp.Model,
@@ -91,10 +86,8 @@ class BranchAndBound:
         match side:
             case 'left':
                 child_var.UB = floor(parent_var.X)
-                print(f"criando novo branch a partir da variável com `{parent_var.VarName} <= {floor(parent_var.X)}`")
             case 'right':
                 child_var.LB = ceil(parent_var.X)
-                print(f"criando novo branch a partir da variável `{parent_var.VarName} >= {ceil(parent_var.X)}`")
             case _:
                 raise ValueError(f"undefined `{side}` child")
 
